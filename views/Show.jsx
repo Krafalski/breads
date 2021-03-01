60d123ddf43f428d17050ac1552e87dc7d373d52
Karolina Rafalski <kr2174@caa.columbia.edu>
2021-03-01 10:12:14 -0500

    pause point

--- views/Show.jsx
+++ views/Show.jsx
@@ -4,7 +4,7 @@ const Default = require('./layouts/Default')
 function Show ({bread, index, title}) {
     return (
       <Default title={title}>
-        <h3>{bread.name}</h3>
+
         <div className="show-container">
           <div className="image-container">
             <div className="image">
@@ -12,16 +12,21 @@ function Show ({bread, index, title}) {
             </div>
           </div>
           <div className="info">
-          <h4>Is made up of {bread.flourType}</h4>
-          <p>
-            and it
-            {
-              bread.hasGluten
-              ? <span> does </span>
-              : <span> does NOT </span>
-            }
-            have gluten.
-          </p>
+            <h3>{bread.name}</h3>
+            <h4>{bread.rating.map(star => {
+              String.fromCodePoint(parseInt(star, 16))
+            })}</h4>
+            <h4>Is made up of {bread.flourType}</h4>
+            <p>
+              and it
+              {
+                bread.hasGluten
+                ? <span> does </span>
+                : <span> does NOT </span>
+              }
+              have gluten.
+            </p>
+
            <div className="actions">
               <form action={`/breads/${index}\?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE"/>
